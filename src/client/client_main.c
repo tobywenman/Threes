09e6cc72243a0247b6c7758f13148ca579544125
toby.wenman <tobywenman@gmail.com>
2026-02-01 22:33:30 +0000

    start thinking about the game client
    This pulls in SDL and starts some state machine stuff

--- src/client/client_main.c
+++ src/client/client_main.c
@@ -1,1 +1,19 @@
-int main(){}
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "client_state.h"
+
+int main()
+{
+    client_state_t* state = init_state();
+
+    if (state != NULL)
+        while (call_scene_func(state)) {}
+
+    free(state);
+
+    return 0;
+}
---
+++ src/client/client_state.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+
+typedef enum scene_e
+{
+    SPLASH,
+    MENU,
+    SERVER_JOIN,
+    GAME,
+    QUIT,
+}scene_e;
+
+typedef struct client_state_t
+{
+    SDL_Window* win;
+    SDL_Renderer* renderer;
+
+    scene_e cur_scene;
+    void* scene_state;
+}client_state_t;
+
+// call the appropriate game scene
+bool call_scene_func(client_state_t* state);
+
+void change_scene(client_state_t* state, scene_e new_scene);
+
+typedef struct splash_state_t
+{
+    SDL_Texture* texture;
+}splash_state_t;
+
+// Each scene has a init, main loop and destructor function
+void splash_init(client_state_t* state);
+void splash_main(client_state_t* state);
+void splash_end(client_state_t* state);
+
+
+void quit_func(client_state_t* state);
+
+
+client_state_t* init_state();
+
+void destroy_state(client_state_t* state);
